b92611a8c361135beecec7eca6ff21c96b348900
Ashutosh Jha <98846260+ashdude1401@users.noreply.github.com>
2024-02-03 14:05:43 +0530

    send_notification

--- frontend/casual_netvorking/lib/main.dart
+++ frontend/casual_netvorking/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Casual Networking',
+      debugShowCheckedModeBanner: false,
       theme: TAppTheme.darkTheme,
       home: WelcomeScreen(),
     );
